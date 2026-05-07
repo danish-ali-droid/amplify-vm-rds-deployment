133081ce3088a13acff6b959b69c8166e482ac2c
Danish-ali <0000danishsh@gmail.com>
2026-05-07 10:54:04 +0500

    adding tst file

---
+++ backend/tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_math_works()
+    {
+        // Ye simple test hamesha pass hoga
+        $this->assertEquals(2, 1 + 1);
+    }
+}
