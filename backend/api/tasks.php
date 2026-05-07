d45c006ae7540733f80cea49eb735956dac6cff5
Danish-ali <0000danishsh@gmail.com>
2026-05-07 10:20:56 +0500

    adding backend code

--- backend/api/tasks.php
+++ backend/api/tasks.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-     exit;
+    exit;
 }
 
 require_once '../config/db.php';
